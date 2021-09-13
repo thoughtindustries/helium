d43692ceed2e3d59c948c47fb1cd1fe944e7f5cb
Rob Schillinger <schillinger.rob@gmail.com>
2021-09-13 15:42:12 +0000

    add windi and rename

---
+++ template_base/vite.config.js
@@ -0,0 +1,13 @@
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import ssr from 'vite-plugin-ssr/plugin';
+import mdx from 'vite-plugin-mdx';
+import WindiCSS from 'vite-plugin-windicss';
+
+const mdxOptions = {
+  remarkPlugins: [],
+  rehypePlugins: []
+};
+
+export default {
+  plugins: [reactRefresh(), ssr(), mdx(mdxOptions), WindiCSS()]
+};
