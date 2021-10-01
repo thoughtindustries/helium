bdb3c5cdba349d47f1ff3e3915c255873082de00
Lu Jiang <lu.jiang@thoughtindustries.com>
2021-10-01 21:41:41 +0000

    feat: add custom tailwind color and font family for branding theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,27 @@ module.exports = {
         '3xl': '2560px'
       },
       colors: {
-        teal: {
-          300: '#8bcfd6',
-          400: '#65bfc9'
+        accent: '#111111',
+        secondary: '#111111',
+        link: '#a9a9a9',
+        'accent-highlight': '#111111',
+        'accent-highlight-background': '#dddddd',
+        'accent-tinted': {
+          70: '#b8b8b8',
+          80: '#cfcfcf',
+          90: '#e7e7e7'
         },
+        'accent-contrast': '#ffffff',
+        'accent-contrast-tinted-background': '#b3b3b3',
+        'accent-hover': '#2b2b2b',
+        'secondary-hover': '#2b2b2b',
+        'secondary-contrast': '#ffffff',
+        'link-hover': '#767676',
       },
       fontFamily: {
-        sans: ['Sintony', 'sans-serif'],
+        primary: ['Sintony', 'Nunito', 'sans-serif'],
+        header: ['Sintony', 'Nunito', 'sans-serif'],
+        secondary: ['Nunito', 'sans-serif']
       },
     },
   },
