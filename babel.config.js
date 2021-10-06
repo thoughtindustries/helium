cdca5ea09f25578fea85839fcd7bc739866dd9a8
Lu Jiang <lu.jiang@thoughtindustries.com>
2021-10-06 20:54:37 +0000

    fix: differentiate builds by env vars

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
+const BABEL_ENV = process.env.BABEL_ENV
+const isCommonJS = BABEL_ENV !== undefined && BABEL_ENV === "cjs"
+const isESM = BABEL_ENV !== undefined && BABEL_ENV === "esm"
+
 module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+    presets: [
+        [
+          "@babel/preset-env",
+          {
+            loose: true,
+            modules: isCommonJS ? "commonjs" : false,
+            targets: {
+              esmodules: isESM ? true : undefined,
+            },
+          },
+        ], 
+        '@babel/preset-react', 
+        '@babel/preset-typescript'
+    ],
 };
