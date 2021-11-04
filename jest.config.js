24caea248919a421388b7624d61fe50350fc7a90
Lu Jiang <89472381+ljiang-ti@users.noreply.github.com>
2021-11-04 13:31:31 -0700

    Cleanup packages (#31)
    * fix: point package files field to src
    
    * fix: remove build scripts from packages
    
    * fix: clean up jest
    
    Co-authored-by: Lu Jiang <lu.jiang@thoughtindustries.com>

--- jest.config.js
+++ jest.config.js
@@ -60,11 +60,7 @@ module.exports = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json"
-    }
-  },
+  // globals: {},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -100,7 +96,7 @@ module.exports = {
   preset: 'ts-jest',
 
   // Run tests from one or more projects
-  projects: ['<rootDir>/packages/*'],
+  // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
   // reporters: undefined,
@@ -141,7 +137,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
--- packages/header/__tests__/header.test.tsx
+++ packages/header/__tests__/header.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { Header, HeaderProps } from "..";
+import { Header, HeaderProps } from "../src";
 
 const setupProps = (alternateTitleDisplay: boolean = false): HeaderProps => ({
   title: "Header title",
