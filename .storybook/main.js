6c8cb08f0760814620a913ace04dd42650395688
Lu Jiang <lu.jiang@thoughtindustries.com>
2021-11-22 17:46:09 +0000

    fix: use postcss v8 in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,16 @@ const path = require('path');
 module.exports = {
   stories: ['../packages/**/stories/*.stories.mdx', '../packages/**/stories/*.stories.tsx'],
   addons: [
-    '@storybook/addon-links', 
-    '@storybook/addon-essentials', 
-    '@storybook/addon-postcss'
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss')
+        }
+      }
+    }
   ],
   typescript: {
     check: true,
