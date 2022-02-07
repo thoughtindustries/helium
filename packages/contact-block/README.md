7828e26a02cd1c703992c88e4860b6ef58367a8c
Jamal-majid-ti <jamal.abdul-majid@thoughtindustries.com>
2022-02-07 15:51:47 +0000

    tailwind, readme, & storybook implemented.

--- packages/contact-block/README.md
+++ packages/contact-block/README.md
@@ -1,11 +1,22 @@
-# `contact-block`
+# `@thoughtindustries/Contact-Block`
 
-> TODO: description
+> a ContactBlock is a block of information used to display contact information, Normaly placed towards the bottom of a web page, above on in the footer. It often consists of information that can be used to contact the sites owners or admins.
+
+## Import component
+
+```
+import { ContactBlock } from '@thoughtindustries/ContactBlock';
+```
 
 ## Usage
 
 ```
-const contactBlock = require('contact-block');
-
-// TODO: DEMONSTRATE API
+<ContactBlock
+  contactDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique metus nec sagittis euismod lorem ipsum forte contiuum" 
+  contactSubtitle="Subtitle"
+  contactName="Jane Jacobs"
+  actionText="Contact"
+   />
 ```
+
+
--- packages/contact-block/src/contact-block.tsx
+++ packages/contact-block/src/contact-block.tsx
@@ -12,54 +12,56 @@ const ContactBlock = (props: ContactBlockProps): JSX.Element => {
     contactName,
     contactSubtitle,
     actionText,
-    actionType
+    contactEmail
   } = props;
-  let { contactEmail } = props;
-  contactEmail = 'mailto:' + contactEmail;
 
   const image = asset && (
-    <span className="ember-view avatar avatar--medium">
-      <img src={asset} alt={contactName} className="ember-view" />
+    <span className="mx-auto mt-0 mb-4 bg-gray-400 text-gray-700 rounded-lg block h-10 relative w-10 leading-7 text-5xl">
+      <img src={asset} alt={contactName} />
     </span>
   );
   const description = contactDescription ? (
-    <p className="contact-block__description"> {contactDescription}</p>
+    <p className="font-medium text-sm leading-7 mb-4"> {contactDescription}</p>
   ) : (
-    <p className="contact-block__description">
+    <p className="font-medium text-sm leading-7 mb-4">
       {' '}
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique metus nec sagittis
       euismod lorem ipsum forte contiuum.
     </p>
   );
   const nameBlock = (
-    <div className="contact-block__name-container">
-      {contactName && <p className="contact-block__name"> {contactName}</p>}
-      {contactSubtitle && <p className="contact-block__subtitle"> {contactSubtitle} </p>}
+    <div className="text-xs mb-4">
+      {contactName && (
+        <p className="font-extrabold my-0 mr-0 ml-1 uppercase tracking-widest"> {contactName}</p>
+      )}
+      {contactSubtitle && <p className="mx-0 mb-0 mt-px"> {contactSubtitle} </p>}
     </div>
   );
   const contactBloacStyle = {
     backgroundColor,
-    textColor
+    color: textColor
   };
 
   const getButton = () => {
     if (actionText && (url || contactEmail)) {
       const anchorProps: { className: string; href?: string; target?: string } = {
-        className: 'btn btn--primary btn--centered contact-block__action',
-        href: url || contactEmail
+        className:
+          'text-white bg-indigo-700 hover:bg-indigo-600  rounded-sm border-solid border cursor-pointer inline-block font-normal text-sm leading-tight py-2 px-5 relative text-center no-underline ease-in-out duration-200 transition font-sans mx-auto mb-0',
+        href: url || 'mailto:' + contactEmail
       };
       if (linkOpenInNewTab) {
         anchorProps.target = '_blank';
       }
-      return <a {...url}>{actionText}</a>;
+      return <a {...anchorProps}>{actionText}</a>;
     }
+    return '';
   };
-  const button = getButton() || '';
+  const button = getButton();
   return (
     <>
-      <div id="ember12482" className="ember-view contact-block" style={contactBloacStyle}>
+      <div className="px-4 py-12 text-center" style={contactBloacStyle}>
         <div className="row">
-          <div className="small-8 medium-6 small-centered columns">
+          <div className="w-8/12 md:w-6/12 float-none mx-auto px-4 relative">
             {image}
             {description}
             {nameBlock}
