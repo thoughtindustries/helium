41064c7daf5048b6bd0b1996800ea2841cb1d642
Rob Schillinger <schillinger.rob@gmail.com>
2022-02-09 20:07:11 +0000

    feat: updates template-base for Thought Industries similarity; removes vite defaults

--- tooling/template-base/pages/index/index.page.jsx
+++ tooling/template-base/pages/index/index.page.jsx
@@ -1,60 +1,99 @@
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
 import { useTranslation } from 'react-i18next';
-import { Counter } from './Counter';
-
+import Layout from '../../components/Layout';
 import { Hero } from '@thoughtindustries/hero';
+import { FeaturedContent, ContentTileStandardLayout } from '@thoughtindustries/featured-content';
+import { hydrateContent, useCatalogQuery } from '@thoughtindustries/content';
+import PropTypes from 'prop-types';
 
 export { Page };
 export { documentProps };
-
-const query = gql`
-  query CompanyDetailsQuery {
-    CompanyDetails {
-      __typename
-      name
-    }
-  }
-`;
 
 const documentProps = {
   title: 'Home Page',
   description: 'The home page'
 };
 
-function Page(pageProps) {
-  const { data } = useQuery(query);
-  const companyName = data && data.CompanyDetails ? data.CompanyDetails.name : 'No name';
+function Page({ appearance, currentUser }) {
+  return (
+    <Layout appearance={appearance} currentUser={currentUser}>
+      <div className="flex flex-col items-start space-y-2">
+        <Hero
+          title="Welcome to Helium!"
+          subtitle="This page is completely rendered in React. Go ahead and edit it at pages/index/index.page.jsx"
+          asset="https://d36ai2hkxl16us.cloudfront.net/thoughtindustries/image/upload/a_exif,c_fill,w_1500/v1426249885/sq5qv0uebvfywkxbw3cc.jpg"
+        />
+        <FeaturedItems />
+      </div>
+    </Layout>
+  );
+}
 
-  const { t } = useTranslation();
-  const translatedCompanyNameString = t('home.meta-title', { companyName });
+Page.propTypes = {
+  appearance: PropTypes.object,
+  currentUser: PropTypes.object
+};
 
-  const authors = ['Jack', 'Jill'];
-  const translatedAuthorString = t('course-group-authors', { count: authors.length });
+const ADD_RESOURCE_TO_QUEUE_MUTATION = gql`
+  mutation AddResourceToQueueMutation($resourceType: ContentKind, $resourceId: ID!) {
+    AddResourceToQueue(resourceType: $resourceType, resourceId: $resourceId)
+  }
+`;
 
-  const { accentColor } = pageProps;
-  const headerStyle = {
-    color: accentColor
+function FeaturedItems() {
+  const { i18n } = useTranslation();
+  const [addResourceToQueue] = useMutation(ADD_RESOURCE_TO_QUEUE_MUTATION);
+  const handleAddedToQueue = item =>
+    item.displayCourse
+      ? addResourceToQueue({ variables: { resourceId: item.displayCourse } }).then()
+      : Promise.resolve(undefined);
+
+  const { data, loading, error } = useCatalogQuery({
+    variables: {
+      query:
+        'courseGroup:5ce1f5cf-4a4e-5e43-a7d7-8b4963b44d63 OR courseGroup:95897223-339b-529b-bc09-03e83fdb0377 OR courseGroup:8c99a587-f64f-5655-9a16-5f016a3a8321 OR courseGroup:15f4c8e7-41f8-517c-832d-d20e7295717e'
+    }
+  });
+
+  const handleClick = (evt, item) => {
+    console.log('clicked!', item);
   };
 
+  let content;
+
+  if (loading) {
+    content = <p>Loading content</p>;
+  }
+  if (error) {
+    content = <p>Error loading content</p>;
+  }
+  if (data) {
+    content = data.CatalogQuery.contentItems.map((item, index) => {
+      const hydratedItem = hydrateContent(i18n, item);
+      const { authors, description, href, ...restItemProps } = hydratedItem;
+      const transformedItem = {
+        ...restItemProps,
+        authors,
+        shortDescription: description && `${description.substring(0, 75)} ...`,
+        linkUrl: href
+      };
+      return <ContentTileStandardLayout.Item key={`item-${index}`} {...transformedItem} />;
+    });
+  }
+
   return (
-    <>
-      <Hero
-        title="Dolor Nullam Mattis Sem"
-        subtitle="Maecenas sed diam eget risus varius blandit sit amet non magna. Praesent commodo cursus magna, vel scelerisque nisl consectetur et."
-        asset="https://d36ai2hkxl16us.cloudfront.net/thoughtindustries/image/upload/a_exif,c_fill,w_800/v1416438573/placeholder_kcjvxm.jpg"
-      />
-      <h1 style={headerStyle}>Welcome to {translatedCompanyNameString}!</h1>
-      <h2>
-        {translatedAuthorString} {authors.join(' and ')}
-      </h2>
-      Testing the build and deploy process (now with meta!).
-      <ul>
-        <li className="text-gray-400">Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
-    </>
+    <div className="px-4">
+      <FeaturedContent>
+        <ContentTileStandardLayout
+          desktopColumnCount={4}
+          headerOptions={{ title: 'Featured Content' }}
+          onAddedToQueue={handleAddedToQueue}
+          onClick={handleClick}
+        >
+          {content}
+        </ContentTileStandardLayout>
+      </FeaturedContent>
+    </div>
   );
 }
