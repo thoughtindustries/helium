bf7a58879c6fad829ea14650e008587adaa17fd1
Lu Jiang <lu.jiang@thoughtindustries.com>
2022-03-02 20:34:54 +0000

    refactor: use content loading dots from code review

--- packages/catalog/src/catalog-loader.tsx
+++ packages/catalog/src/catalog-loader.tsx
@@ -1,17 +1,6 @@
 import React, { createRef, ReactElement, useEffect, RefObject, useState } from 'react';
+import { LoadingDots } from '@thoughtindustries/content';
 import { useCatalogLoader } from './core';
-
-const LoadingDots = (): JSX.Element => {
-  const firstDotStyles = { animationDelay: '-0.32s' };
-  const secondDotStyles = { animationDelay: '-0.16s' };
-  return (
-    <div className="flex items-center justify-center space-x-10">
-      <div className="animate-ping w-5 h-5 bg-gray-700 rounded-full" style={firstDotStyles}></div>
-      <div className="animate-ping w-5 h-5 bg-gray-700 rounded-full" style={secondDotStyles}></div>
-      <div className="animate-ping w-5 h-5 bg-gray-700 rounded-full"></div>
-    </div>
-  );
-};
 
 interface CatalogLoaderProps {
   children: (targetRef: RefObject<HTMLDivElement>) => ReactElement;
