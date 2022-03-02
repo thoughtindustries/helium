0ad1dce4c4ff0d709db238aa2b7ae752004165f8
Lu Jiang <lu.jiang@thoughtindustries.com>
2022-03-02 23:47:34 +0000

    feat: add test and fix export

--- packages/content/src/graphql/queries/index.ts
+++ packages/content/src/graphql/queries/index.ts
@@ -2,6 +2,7 @@ export * from './CatalogContent.generated';
 export * from './CatalogMetaFragment.generated';
 export * from './CatalogQuery.generated';
 export * from './ContentFragment.generated';
+export * from './LocationFragment.generated';
 export * from './QueryContents.generated';
 export * from './RssItems.generated';
 export * from './UserRecentContent.generated';
