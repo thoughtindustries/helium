b204b9eed1d6a08350f8a5a6fdf925e99c02fc21
Blake Reimer <blake.reimer@Blake-Reimer-MacBook-Pro.local>
2022-04-28 19:11:36 +0000

    feat: init

---
+++ packages/redemption/src/components/redemption.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const Redemption = (): JSX.Element => {
+  const { t } = useTranslation();
+
+  return <></>;
+};
+
+Redemption.displayName = 'Redemption';
+export default Redemption;
