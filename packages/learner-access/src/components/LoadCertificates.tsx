7c0eadfeb372cb11300d4285ff84b646f9068a6e
Jamal-majid-ti <jamal.abdul-majid@thoughtindustries.com>
2022-05-06 21:42:25 +0000

    fix: update feilds for queries

--- packages/learner-access/src/components/LoadCertificates.tsx
+++ packages/learner-access/src/components/LoadCertificates.tsx
@@ -22,12 +22,19 @@ const LoadCertificates = ({
   if (error) return error;
   return (
     <>
-      <div className="inline-block">
-        <div className="top-2/4 left-1/2 mr-0 mb-0 -mt-5 -ml-16 absolute">
+      <div className="inline-block w-full h-auto">
+        <div className=" mr-0 mb-0 -mt-5">
           {loading ? (
             <LoadingDots />
           ) : (
-            <div className="text-center border-solid border-b text-base py-3 px-4 text-gray-400 border-gray-light"></div>
+            <div className="text-center border-solid border-b text-base py-3 px-4 text-gray-400 border-gray-light">
+              <div className="float-left px-4 relative w-full">
+                <button className="btn btn--primary btn--medium" data-ember-action="4831">
+                  <i className="icon-upload" aria-label="upload"></i>
+                  <span id="i18n-60">Upload Third-Party Certificate</span>
+                </button>
+              </div>
+            </div>
           )}
         </div>
       </div>
