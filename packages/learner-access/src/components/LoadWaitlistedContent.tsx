9a28f4f92b29483e9137313ef96aa0d0416d2448
Lu Jiang <lu.jiang@thoughtindustries.com>
2022-07-06 00:23:15 +0000

    fix: type ref in waitlisted tab

--- packages/learner-access/src/components/LoadWaitlistedContent.tsx
+++ packages/learner-access/src/components/LoadWaitlistedContent.tsx
@@ -6,54 +6,54 @@ import {
 } from '@thoughtindustries/content';
 
 const LoadWaitlist = (): JSX.Element => {
-  const { data, loading, error }: any = useUserWaitlistQuery({
-    variables: {}
+  const { data, loading, error } = useUserWaitlistQuery({
+    variables: {},
+    fetchPolicy: 'network-only',
+    ssr: false
   });
   const [unenrollFromWaitlistMutation] = useUnenrollFromWaitlistMutation();
 
   console.log('data from child', data);
-  if (error) return error;
+  if (loading) return <LoadingDots />;
+  if (error) return <>{error.message}</>;
+  if (!data || !data.UserWaitlist) return <></>;
   return (
     <>
-      {loading ? (
-        <LoadingDots />
-      ) : (
-        data.WaitlistedContentForCurrentUser.map((item: any) => {
-          return (
-            <div
-              key={item.id}
-              className="border-solid p-4 text-black-light border-gray-light px-4 py-[0.5rem] odd:bg-white-mid border-b last:border-b-0"
-            >
-              <div className="my-0 mx-auto max-w-full w-full">
-                <div className="grid items-center grid-cols-12 gap-4">
-                  <div className="col-span-4">
-                    <button className="btn btn--link btn--inherit-font dashboard-access-list-item-expander">
-                      <span className="dashboard-access-list-item-expander__title text-gray-mid">
-                        {item.title && item.title}
-                      </span>
-                    </button>
-                  </div>
+      {data.UserWaitlist.map((item: any) => {
+        return (
+          <div
+            key={item.id}
+            className="border-solid p-4 text-black-light border-gray-light px-4 py-[0.5rem] odd:bg-white-mid border-b last:border-b-0"
+          >
+            <div className="my-0 mx-auto max-w-full w-full">
+              <div className="grid items-center grid-cols-12 gap-4">
+                <div className="col-span-4">
+                  <button className="btn btn--link btn--inherit-font dashboard-access-list-item-expander">
+                    <span className="dashboard-access-list-item-expander__title text-gray-mid">
+                      {item.title && item.title}
+                    </span>
+                  </button>
+                </div>
 
-                  <div className="col-span-2"></div>
+                <div className="col-span-2"></div>
 
-                  <div className="col-span-3 text-gray-mid">
-                    <strong>{item.contentTypeLabel && item.contentTypeLabel}</strong>
-                  </div>
+                <div className="col-span-3 text-gray-mid">
+                  <strong>{item.contentTypeLabel && item.contentTypeLabel}</strong>
+                </div>
 
-                  <div className="col-start-11 col-span-2 text-right">
-                    <button
-                      onClick={() => unenrollFromWaitlistMutation({ variables: { id: item.id } })}
-                      className="bg-active-blue text-white rounded-sm cursor-pointer inline-block font-normal text-xs m-0 py-[0.15rem] px-4 relative text-center no-underline ease-in-out border-active-blue font-sans transition duration-200 leading-5"
-                    >
-                      Unenroll from Waitlist
-                    </button>
-                  </div>
+                <div className="col-start-11 col-span-2 text-right">
+                  <button
+                    onClick={() => unenrollFromWaitlistMutation({ variables: { id: item.id } })}
+                    className="bg-active-blue text-accent-contrast bg-accent rounded-sm cursor-pointer inline-block font-normal text-xs m-0 py-[0.15rem] px-4 relative text-center no-underline ease-in-out border-active-blue font-sans transition duration-200 leading-5"
+                  >
+                    Unenroll from Waitlist
+                  </button>
                 </div>
               </div>
             </div>
-          );
-        })
-      )}
+          </div>
+        );
+      })}
     </>
   );
 };
