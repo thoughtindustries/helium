20d576ed1980d02d33ed546e560660ad8d76398f
Blake Reimer <blake.reimer@Blake-Reimer-MacBook-Pro.local>
2022-07-07 17:44:20 +0000

    fix: remove list state from code-list

--- packages/redemption/src/components/code-list.tsx
+++ packages/redemption/src/components/code-list.tsx
@@ -1,14 +1,14 @@
-import React, { ReactNode, useState } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import CodeBox from './code-box';
 import { ResponseProps } from './types';
 
 const CodeList = ({ setResponse }: ResponseProps): JSX.Element => {
   const { t } = useTranslation();
-  const [count, setCount] = useState<number>(0);
-  const [list, setList] = useState<ReactNode[]>([
-    <CodeBox key={`code-box${count}`} setResponse={setResponse} />
-  ]);
+  const [count, setCount] = useState<number>(1);
+  const list = Array.from(Array(count), (_, i) => (
+    <CodeBox key={`code-box-${i}`} setResponse={setResponse} />
+  ));
 
   return (
     <div>
@@ -18,7 +18,6 @@ const CodeList = ({ setResponse }: ResponseProps): JSX.Element => {
         type="button"
         onClick={() => {
           setCount(count + 1);
-          setList([...list, <CodeBox key={`code-box${count + 1}`} setResponse={setResponse} />]);
         }}
       >
         {t('redemption-code.add-redemption-code')}
