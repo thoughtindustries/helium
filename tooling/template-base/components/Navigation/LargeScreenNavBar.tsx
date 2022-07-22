9fe0f7eeed9c33feeb03047ef659728dceaaa9ec
Alex O'Neill <alex.oneill@alex-oneill-mbp.home>
2022-07-22 10:15:56 +0100

    feat: add catalogAggregation to catalog page

---
+++ tooling/template-base/components/Navigation/LargeScreenNavBar.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useState } from 'react';
+import Logo from './Logo';
+import icon from '../../renderer/menubar.png';
+import avatar from '../../renderer/avatar.png';
+import xicon from '../../renderer/xicon.png';
+import MobileMenu from './MobileMenu';
+import dropDownClosed from '../../renderer/dropDownClosed.png';
+import dropDownOpen from '../../renderer/dropDownOpen.png';
+
+export default function LargeSceenNavBar() {
+  const [navbar, setNavbar] = useState(false);
+
+  return (
+    <nav className="w-full bg-white shadow py-4">
+      <div className="justify-between px-4 md:items-center md:flex">
+        <div>
+          <div className="flex items-center justify-between md:block">
+            {/* logo */}
+            <div className="">
+              <Logo />
+            </div>
+            <div className="md:hidden"></div>
+          </div>
+        </div>
+        <div className={`flex my-auto space-x-6 mx-auto md:block ${navbar ? 'block' : 'hidden'}`}>
+          <ul className="items-center justify-between space-y-3 pt-4 md:space-y-0 md:flex md:space-x-6 w-full">
+            <li className="md:hover:bg-white hover:bg-slate-100 rounded pl-2 py-1.5">
+              <a href="/">My Dashboard</a>
+            </li>
+            <li className="md:hover:bg-white hover:bg-slate-100 rounded pl-2 py-2">
+              <a href="/catalog">Catalog</a>
+            </li>
+            <li className="md:hover:bg-white hover:bg-slate-100 rounded pl-2 py-2">
+              <a href="/help">Help</a>
+            </li>
+            <hr></hr>
+          </ul>
+        </div>
+
+        <div className="hidden md:block">
+          <button className="hidden md:block">
+            {navbar ? (
+              <button id="icon" type="submit" className="block" onClick={() => setNavbar(!navbar)}>
+                <div className="flex items-center space-x-3 mr-3">
+                  <img src={avatar} />
+                  <img src={dropDownOpen} />
+                </div>
+              </button>
+            ) : (
+              <button id="icon" type="submit" className="block" onClick={() => setNavbar(!navbar)}>
+                <div className="flex items-center space-x-3 mr-3">
+                  <img src={avatar} />
+                  <img src={dropDownClosed} />
+                </div>
+              </button>
+            )}
+          </button>
+        </div>
+      </div>
+      {/* mobile menu */}
+      <div
+        className={`${
+          navbar ? 'block' : 'hidden'
+        } absolute right-0 p-5 mt-6 mr-2 w-96 rounded-md shadow-lg bg-white`}
+      >
+        <MobileMenu />
+      </div>
+    </nav>
+  );
+}
