f84bad88b83f98909c4f0f298cab741d0485fea2
Blake Reimer <blake.reimer@node-146-1-168-192.int.nedernet.net>
2022-07-26 14:42:03 +0000

    feat: update snapshot

--- packages/registration-form/__tests__/registration.test.tsx
+++ packages/registration-form/__tests__/registration.test.tsx
@@ -55,41 +55,39 @@ describe('@thoughtindustries/registration', () => {
       await waitFor(() => new Promise(res => setTimeout(res, 0)));
       expect(container).toMatchInlineSnapshot(`
         <div>
-          <div
+          <form
             class="mx-4 md:mx-40 text-center self-center"
           >
-            <form>
+            <div>
               <div>
-                <div>
-                  <input
-                    class="p-4 text-sm w-full md:w-2/3 ring-1 ring-gray-300 ring-inset shadow-inner focus:outline-none focus:ring-gray-500 mb-4"
-                    placeholder="redemption-code.placeholder"
-                  />
-                  <button
-                    class="text-white bg-indigo-700 disabled:bg-indigo-300 disabled:cursor-default hover:bg-indigo-600 inline-block font-normal text-sm no-underline py-4 w-full md:w-1/3 rounded-md md:rounded-l-none mb-4"
-                    type="button"
-                  >
-                    redemption-code.validate
-                  </button>
-                </div>
+                <input
+                  class="p-4 text-sm w-full md:w-2/3 ring-1 ring-gray-300 ring-inset shadow-inner focus:outline-none focus:ring-gray-500 mb-4"
+                  placeholder="redemption-code.placeholder"
+                />
                 <button
-                  class="flex justify-left text-indigo-700 text-sm"
+                  class="text-white bg-indigo-700 disabled:bg-indigo-300 disabled:cursor-default hover:bg-indigo-600 inline-block font-normal text-sm no-underline py-4 w-full md:w-1/3 rounded-md md:rounded-l-none mb-4"
                   type="button"
                 >
-                  redemption-code.add-redemption-code
+                  redemption-code.validate
                 </button>
               </div>
-            </form>
+              <button
+                class="flex justify-left text-indigo-700 text-sm"
+                type="button"
+              >
+                redemption-code.add-redemption-code
+              </button>
+            </div>
             <div
               class="w-full border-t border-gray-200 my-4"
             />
             <button
-              class="text-white bg-indigo-700 hover:bg-indigo-600 inline-block font-normal text-sm text-center no-underline py-2 w-full md:w-1/4 rounded-md"
+              class="text-white bg-indigo-700 hover:bg-indigo-600 inline-block font-normal text-sm text-center no-underline py-2 w-full md:w-1/4 rounded-md disabled:bg-indigo-300 disabled:cursor-default"
               type="button"
             >
               redemption-code.redeem-code-preloaded
             </button>
-          </div>
+          </form>
         </div>
       `);
     });
