22ed39a8ac60d8f9c6084d1241e4ad0801566e23
Alex O'Neill <alex.oneill@alex-oneill-mbp.home>
2022-07-29 17:27:55 +0100

    feat: remove ProductInformationCards.tsx

--- tooling/template-base/pages/index/index.page.tsx
+++ tooling/template-base/pages/index/index.page.tsx
@@ -5,7 +5,7 @@ import Footer from '../../components/Footer/Footer';
 import Hero from '../../components/Hero';
 import HomepageNavbar from '../../components/Navigation/HomepageNavbar';
 import CallToActionWithLinks from '../../components/CallToActionWithLinks';
-import ProductInformationCards from '../../components/ProductInformationCards';
+import CallToActionParagraphs from '../../components/CallToActionParagraphs';
 
 export { Page };
 export { documentProps };
@@ -42,17 +42,32 @@ function Page() {
           }
         ]}
       />
-      <ProductInformationCards
+      <CallToActionParagraphs
         heading="Your source for learning, community and success."
         subheading="Find products, resources, and programs for every stage of your business journey."
-        leftParagraphHeading="Learning Tools and Resources"
-        leftParagraphContent=" Our goal is to help you become successful by providing learning materials to improve yourself or your company on various topics, such as marketing, business, finance and design."
-        leftParagraphButtonText="View catalog"
-        leftParagraphButtonUrl="/catalog"
-        rightParagraphHeading="World Class Content"
-        rightParagraphContent=" Our goal is to help you become successful by providing learning materials to improve yourself or your company on various topics, such as marketing, business, finance and design."
-        rightParagraphButtonText="View catalog"
-        rightParagraphButtonUrl="/catalog"
+        paragraphItems={[
+          {
+            heading: 'Learning Tools and Resources',
+            content:
+              'Our goal is to help you become successful by providing learning materials to improve yourself or your company on various topics, such as marketing, business, finance and design.',
+            buttonText: 'View catalog',
+            buttonUrl: '/catalog'
+          },
+          {
+            heading: 'World Class Content',
+            content:
+              'Our content is curated for you. The Academy is focused on your everyday tasks and the goals you want to achieve.',
+            buttonText: 'View catalog',
+            buttonUrl: '/catalog'
+          },
+          {
+            heading: 'World Class Content',
+            content:
+              'Our content is curated for you. The Academy is focused on your everyday tasks and the goals you want to achieve.',
+            buttonText: 'View catalog',
+            buttonUrl: '/catalog'
+          }
+        ]}
       />
       <CTA
         headline="Are you ready to learn?"
