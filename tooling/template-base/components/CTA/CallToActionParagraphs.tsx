f72d44a5d2d246fdeb57912ca55d26abc8b02ebf
Alex O'Neill <alex.oneill@alex-oneill-mbp.home>
2022-08-05 16:17:52 +0100

    fix: add key to CTAParagraphs

--- tooling/template-base/components/CTA/CallToActionParagraphs.tsx
+++ tooling/template-base/components/CTA/CallToActionParagraphs.tsx
@@ -75,7 +75,11 @@ const CallToActionParagraphs = (props: {
   };
 
   const listItems = props.paragraphItems.map((item, i) => {
-    return i % 2 === 0 ? <RightAlignedSection item={item} /> : <LeftAlignedSection item={item} />;
+    return i % 2 === 0 ? (
+      <RightAlignedSection item={item} key={i} />
+    ) : (
+      <LeftAlignedSection item={item} key={i} />
+    );
   });
 
   return (
