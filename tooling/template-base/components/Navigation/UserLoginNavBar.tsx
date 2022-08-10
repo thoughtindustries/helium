0778d0494acfdedb498a33655ec94207ccbb2613
Alex O'Neill <alex.oneill@Alex-ONeill-MacBook-Pro.local>
2022-08-10 15:36:59 +0200

    fix: removing unused items in renderer

--- tooling/template-base/components/Navigation/UserLoginNavBar.tsx
+++ tooling/template-base/components/Navigation/UserLoginNavBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import Logo from '../Logo/Logo';
-import icon from '../../renderer/vector.svg';
+import icon from '../../renderer/hamburger.svg';
 import xicon from '../../renderer/xicon.svg';
 
 export default function UserLoginNavBar() {
@@ -14,7 +14,7 @@ export default function UserLoginNavBar() {
           <div className="flex items-center justify-between md:block">
             {/* logo */}
             <div className="">
-              <Logo />
+              <Logo logoLink="" style="" />
             </div>
             <div className="md:hidden">
               <div
