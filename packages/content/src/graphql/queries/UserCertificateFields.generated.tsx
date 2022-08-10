e5c98976260dad60354e2fe5592c1104582f2ee4
Alex O'Neill <alex.oneill@Alex-ONeill-MacBook-Pro.local>
2022-08-10 11:12:42 +0200

    feat: merge packages/content/src/graphql/queries/ from learner-access branch

---
+++ packages/content/src/graphql/queries/UserCertificateFields.generated.tsx
@@ -0,0 +1,81 @@
+import * as Types from '../global-types';
+
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
+export type UserCertificateFieldsQueryVariables = Types.Exact<{ [key: string]: never }>;
+
+export type UserCertificateFieldsQuery = {
+  __typename?: 'Query';
+  UserCertificateFields?: Array<{
+    __typename?: 'CertificateField';
+    id: string;
+    type: Types.CertificateFieldType;
+    label: string;
+    awardTypeId?: string;
+    awardType?: { __typename?: 'AwardType'; id: string; pluralLabel?: string };
+  }>;
+};
+
+export const UserCertificateFieldsDocument = gql`
+  query UserCertificateFields {
+    UserCertificateFields {
+      id
+      type
+      label
+      awardTypeId
+      awardType {
+        id
+        pluralLabel
+      }
+    }
+  }
+`;
+
+/**
+ * __useUserCertificateFieldsQuery__
+ *
+ * To run a query within a React component, call `useUserCertificateFieldsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useUserCertificateFieldsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useUserCertificateFieldsQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useUserCertificateFieldsQuery(
+  baseOptions?: Apollo.QueryHookOptions<
+    UserCertificateFieldsQuery,
+    UserCertificateFieldsQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<UserCertificateFieldsQuery, UserCertificateFieldsQueryVariables>(
+    UserCertificateFieldsDocument,
+    options
+  );
+}
+export function useUserCertificateFieldsLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    UserCertificateFieldsQuery,
+    UserCertificateFieldsQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<UserCertificateFieldsQuery, UserCertificateFieldsQueryVariables>(
+    UserCertificateFieldsDocument,
+    options
+  );
+}
+export type UserCertificateFieldsQueryHookResult = ReturnType<typeof useUserCertificateFieldsQuery>;
+export type UserCertificateFieldsLazyQueryHookResult = ReturnType<
+  typeof useUserCertificateFieldsLazyQuery
+>;
+export type UserCertificateFieldsQueryResult = Apollo.QueryResult<
+  UserCertificateFieldsQuery,
+  UserCertificateFieldsQueryVariables
+>;
---
+++ packages/content/src/graphql/queries/UserWaitlist.generated.tsx
@@ -0,0 +1,74 @@
+import * as Types from '../global-types';
+
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
+export type UserWaitlistQueryVariables = Types.Exact<{ [key: string]: never }>;
+
+export type UserWaitlistQuery = {
+  __typename?: 'Query';
+  UserWaitlist?: Array<{
+    __typename?: 'Content';
+    id: string;
+    contentTypeLabel?: string;
+    title?: string;
+    kind?: Types.ContentKind;
+    slug: string;
+    displayCourse?: string;
+    displayCourseSlug?: string;
+  }>;
+};
+
+export const UserWaitlistDocument = gql`
+  query UserWaitlist {
+    UserWaitlist {
+      id
+      contentTypeLabel
+      title
+      kind
+      slug
+      displayCourse
+      displayCourseSlug
+    }
+  }
+`;
+
+/**
+ * __useUserWaitlistQuery__
+ *
+ * To run a query within a React component, call `useUserWaitlistQuery` and pass it any options that fit your needs.
+ * When your component renders, `useUserWaitlistQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useUserWaitlistQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useUserWaitlistQuery(
+  baseOptions?: Apollo.QueryHookOptions<UserWaitlistQuery, UserWaitlistQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<UserWaitlistQuery, UserWaitlistQueryVariables>(
+    UserWaitlistDocument,
+    options
+  );
+}
+export function useUserWaitlistLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<UserWaitlistQuery, UserWaitlistQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<UserWaitlistQuery, UserWaitlistQueryVariables>(
+    UserWaitlistDocument,
+    options
+  );
+}
+export type UserWaitlistQueryHookResult = ReturnType<typeof useUserWaitlistQuery>;
+export type UserWaitlistLazyQueryHookResult = ReturnType<typeof useUserWaitlistLazyQuery>;
+export type UserWaitlistQueryResult = Apollo.QueryResult<
+  UserWaitlistQuery,
+  UserWaitlistQueryVariables
+>;
