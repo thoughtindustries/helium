413900a23374145567aa01117dadec42c01a5a4e
Alex O'Neill <alex.oneill@alex-oneill-mbp.home>
2022-08-15 16:49:46 +0100

    feat: add dropdown functionality to dashboard page

--- tooling/template-base/components/LearnerAccess/LoadUserLeaning.tsx
+++ tooling/template-base/components/LearnerAccess/LoadUserLeaning.tsx
@@ -4,6 +4,7 @@ import listViewSelector from '../../renderer/listViewSelector.svg';
 import gridNotSelected from '../../renderer/gridNotSelected.svg';
 import gridSelected from '../../renderer/gridSelected.svg';
 import dropDownClosed from '../../renderer/dropDownClosed.svg';
+import dropDownOpen from '../../renderer/dropDownOpen.svg';
 
 import { LoadedComponentProps } from '@thoughtindustries/learner-access/src/types';
 import {
@@ -58,38 +59,97 @@ const LoadUserLearning = ({ query, kind, sort }: LoadedComponentProps): JSX.Elem
 
   const DisplayListView = ({ item }: ContentUiProps) => {
     const [showContent, setShowContent] = useState<boolean>(false);
+    const [listViewDropDown, setListViewDropDown] = useState(false);
+
+    const { data, loading, error } = useUserCourseCompletionProgressQuery({
+      variables: {
+        id: item.id
+      }
+    });
+
+    console.log(data);
 
     return (
       // list flex container
-      <div className="flex flex-col w-full">
-        <div className="flex flex-row basis-8/12">
+      <>
+        <div className="flex flex-col w-full">
           <div className="flex flex-row basis-8/12">
-            {/* course image */}
-            <div className="py-4 pl-6 basis-4/12">
-              {item.asset ? (
-                <img src={item.asset} className="rounded-md" />
-              ) : (
-                <img src={dashboardDefault} className="rounded-md" />
-              )}
+            <div className="flex flex-row basis-8/12">
+              {/* course image */}
+              <div className="py-4 pl-6 basis-4/12">
+                {item.asset ? (
+                  <img src={item.asset} className="rounded-md" />
+                ) : (
+                  <img src={dashboardDefault} className="rounded-md" />
+                )}
+              </div>
+              {/* course title */}
+              <div className="flex items-center px-6 text-sm font-semibold font-primary">
+                {item.title}
+              </div>
             </div>
-            {/* course title */}
-            <div className="flex items-center px-6 text-sm font-semibold font-primary">
-              {item.title}
-            </div>
-          </div>
-          <div className="flex flex-row basis-4/12 justify-between">
-            {/* course completion */}
-            <div className="flex items-center pr-6 text-sm font-semibold font-primary text-gray-500">
-              25% complete
-            </div>
-            {/* continue button */}
-            <div className="flex items-center text-sm font-semibold text-blue-400">Continue</div>
-            {/* drop down menu */}
-            <div className="flex items-center pr-4">
-              <img src={dropDownClosed} className="h-2" />
+            <div className="flex flex-row basis-4/12 justify-between">
+              {/* course completion */}
+              <div className="flex items-center pr-6 text-sm font-semibold font-primary text-gray-500">
+                {/* {data} */}
+              </div>
+              {/* continue button */}
+              <div className="flex items-center text-sm font-semibold text-blue-400">Continue</div>
+              {/* drop down menu */}
+              <button
+                className="flex items-center pr-4"
+                onClick={() => setListViewDropDown(!listViewDropDown)}
+              >
+                {listViewDropDown ? (
+                  <img src={dropDownOpen} className="h-2" />
+                ) : (
+                  <img src={dropDownClosed} className="h-2" />
+                )}
+              </button>
             </div>
           </div>
         </div>
+        {/* listDisplayDropDown */}
+        {listViewDropDown && <ListDisplayDropDown item={item} />}
+      </>
+    );
+  };
+
+  const ListDisplayDropDown = ({ item }: ContentUiProps) => {
+    return (
+      // {/* list drop down */}
+      <div className="flex flex-col bg-slate-50 p-6 space-y-6 mb-5">
+        {/* article and duration */}
+        <div className="flex flex-row">
+          <div className="py-1 px-3 rounded-full bg-green-200">
+            <div className="text-sm font-semibold">{item.contentTypeLabel}</div>
+          </div>
+          <div className="flex items-center text-gray-500 px-4">|</div>
+          <div className="flex items-center text-gray-500 font-sm font-semibold">30 mins</div>
+        </div>
+        {/* course information */}
+        <div className="flex flex-row justify-between gap-4">
+          <div className="flex flex-grow pl-6 py-6 bg-white rounded-md">
+            <div className="flex flex-col">
+              <div className="text-sm text-gray-500">Passed Assessments</div>
+              <div className="text-3xl font-bold pt-2">0/1</div>
+            </div>
+          </div>
+          <div className="flex flex-grow pl-6 py-6 bg-white rounded-md">
+            <div className="flex flex-col">
+              <div className="text-sm text-gray-500">Required Pages Viewed</div>
+              <div className="text-3xl font-bold pt-2">4/6</div>
+            </div>
+          </div>
+          <div className="flex flex-grow pl-6 py-6 bg-white rounded-md">
+            <div className="flex flex-col">
+              <div className="text-sm text-gray-500">Collaboration</div>
+              <div className="text-3xl font-bold pt-2">2</div>
+            </div>
+          </div>
+        </div>
+        {/* course description */}
+        <div className="text-sm font-semibold">{item.description}</div>
       </div>
     );
   };
@@ -182,7 +242,7 @@ const LoadUserLearning = ({ query, kind, sort }: LoadedComponentProps): JSX.Elem
         // list flex container
         <div className="flex flex-col w-full">
           {/* title and progress */}
-          <div className="flex flex-row bg-slate-50 px-6 py-3">
+          <div className="flex flex-row bg-slate-50 px-6 py-3 rounded-t-md">
             <div className="text-sm font-semibold basis-8/12">Title</div>
             <div className="text-sm font-semibold basis-4/12">Progress</div>
           </div>
