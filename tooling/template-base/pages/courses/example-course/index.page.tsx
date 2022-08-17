9679dfb5556a229effbeb8f405ad65cb085cdec1
Robert Schillinger <robert.schillinger@thoughtindustries.com>
2022-08-17 14:16:18 +0000

    refactor: extract components

--- tooling/template-base/pages/courses/example-course/index.page.tsx
+++ tooling/template-base/pages/courses/example-course/index.page.tsx
@@ -1,54 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Topic1 from './topics/01-test-topic.page.mdx';
 import Topic2 from './topics/02-second-test-topic.page.mdx';
 import Topic3 from './topics/03-test-topic.page.mdx';
-import { usePageContext } from '../../../renderer/usePageContext';
+import CourseLayout from './../../../components/CourseLayout';
 
 export { Page };
 
 function Page() {
-  const pageContext = usePageContext();
-  let pageQuery = 1;
-
-  const { search } = pageContext.urlParsed;
-  if (search?.page && parseInt(search.page)) {
-    pageQuery = parseInt(search.page);
-  }
-
   const coursePages = [<CoursePage1 key={1} />, <CoursePage2 key={2} />];
-  const [currentPage, setCurrentPage] = useState<number>(pageQuery - 1);
-  const currentView = coursePages[currentPage];
 
-  const handleForward = () => {
-    if (currentPage < coursePages.length - 1) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handleBackward = () => {
-    if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  return (
-    <div className="flex flex-row p-10">
-      <div>
-        <Sidebar pages={coursePages} setPage={setCurrentPage} />
-      </div>
-      <div className="flex flex-col grow items-center">
-        <div className="flex grow justify-center">{currentView}</div>
-        <div>
-          <button className="p-5" onClick={handleBackward}>
-            Back
-          </button>
-          <button className="p-5" onClick={handleForward}>
-            Forward
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  return <CourseLayout title="Example Course" coursePages={coursePages} />;
 }
 
 function CoursePage1() {
@@ -71,21 +32,3 @@ function CoursePage2() {
 }
 
 CoursePage2.label = 'Chapter 2';
-
-function Sidebar({ pages, setPage }: { pages: JSX.Element[]; setPage: (page: number) => void }) {
-  const pageLabels = pages.map(page => page.type.label);
-
-  return (
-    <ul>
-      {pageLabels.map((label, i) => {
-        return (
-          <li key={i}>
-            <button onClick={() => setPage(i)} key={i}>
-              {label}
-            </button>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
