942dca5f3c233571aa1cd7c4dc80a3b10e790e4f
Alex O'Neill <alex.oneill@Alex-ONeill-MBP.station>
2022-08-17 11:26:54 +0100

    feat: removing props from sign in comp

--- tooling/template-base/components/SigninPage.tsx
+++ tooling/template-base/components/SigninPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from './Logo';
+import Logo from './Logo/Logo';
 
 const SigninPage = (props: {
   heading: string;
@@ -15,51 +15,48 @@ const SigninPage = (props: {
   return (
     <div className="flex flex-col items-center py-12 px-8 space-y-4">
       {/* logo component */}
-      <Logo logoLink="/" style="h-12 w-12" />
+      <Logo size="large" />
       {/* heading */}
-      <div className="text-3xl font-bold font-secondary">{props.heading}</div>
-      {/* subheading */}
-      <div className="font-primary text-gray-500 text-sm text-center sm:w-96 w-80">
-        {props.subHeading}
-      </div>
+      <div className="text-3xl font-bold font-secondary">Sign into your account</div>
       {/* Card Element */}
       <div className="p-8 bg-white rounded-md space-y-6 font-primary sm:w-96">
         {/* field 1 */}
         <div>
-          <label className="text-sm font-semibold text-left">{props.field1}</label>
+          <label className="text-sm font-semibold text-left">Email address</label>
           <input
             type="text"
-            id={props.field1}
+            id="Email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder={props.field1Placeholder}
+            placeholder=""
             required
           ></input>
         </div>
         {/* field 2 optional */}
-        {props.field2 && (
-          <div>
-            <label className="text-sm font-semibold text-left">{props.field2}</label>
-            <input
-              type="text"
-              id={props.field2}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-            ></input>
-          </div>
-        )}
+        <div>
+          <label className="text-sm font-semibold text-left">Password</label>
+          <input
+            type="text"
+            id="Password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          ></input>
+        </div>
         {/* sign in button */}
-        <form action={props.primaryButtonUrl}>
+        <form action="/signin">
           <button
             type="submit"
             className="h-12 p-2.5 px-4 bg-indigo-800 hover:bg-blue-500 rounded-md w-full"
           >
-            <div className="text-sm font-semibold text-white">{props.primaryButtonText}</div>
+            <div className="text-sm font-semibold text-white">Sign in</div>
           </button>
         </form>
-        <form action={props.secondaryButtonUrl}>
-          <div className="p-8 text-indigo-800 hover:text-blue-500 text-center font-semibold">
-            {props.secondaryButtonText}
-          </div>
+        <form action="/forgotpassword" className="items-center">
+          <button
+            type="submit"
+            className="p-8 w-full text-indigo-800 hover:text-blue-500 text-center font-semibold justify-center"
+          >
+            Forgot Password
+          </button>
         </form>
       </div>
     </div>
