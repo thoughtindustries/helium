b295735aef74d16425796ff6e29fdf22ba0e1333
Robert Schillinger <robert.schillinger@thoughtindustries.com>
2022-08-18 19:47:00 +0000

    fix: revert mdx additions to helium-server

--- tooling/helium-server/src/vite-config/vite.config.ts
+++ tooling/helium-server/src/vite-config/vite.config.ts
@@ -1,22 +1,11 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import ssr from 'vite-plugin-ssr/plugin';
 
-const tiConfig = async (): Promise<Record<any, any>> => {
-  const remarkFrontmatter = await import('remark-frontmatter');
-  const remarkMdxFrontmatter = await import('remark-mdx-frontmatter');
-  const mdx = await import('@mdx-js/rollup');
-
-  const mdxOptions = {
-    remarkPlugins: [remarkFrontmatter.default, remarkMdxFrontmatter.default],
-    rehypePlugins: []
-  };
-
-  return {
-    plugins: [reactRefresh(), ssr(), mdx.default(mdxOptions)],
-    optimizeDeps: {
-      include: ['dayjs', 'universal-cookie']
-    }
-  };
+const tiConfig: Record<any, any> = {
+  plugins: [reactRefresh(), ssr()],
+  optimizeDeps: {
+    include: ['dayjs', 'universal-cookie']
+  }
 };
 
 export default tiConfig;
