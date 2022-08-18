66b5aaa20c2ddcd6a64a564a7e66d591a156245a
Alex O'Neill <alex.oneill@alex-oneill-mbp.home>
2022-08-18 12:10:49 +0100

    feat: refactor of sign in button

--- tooling/template-base/components/Signin/SigninPage.tsx
+++ tooling/template-base/components/Signin/SigninPage.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../Logo/Logo';
 
-const signInHandler = () => {
-  console.log('Login Mutation Here');
-};
-
 const SigninPage = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const signInHandler = () => {
+    console.log('Login Mutation Here');
+  };
+
   return (
     <div className="flex flex-col items-center py-12 px-8 space-y-4">
       {/* logo component */}
@@ -14,29 +19,28 @@ const SigninPage = () => {
       <div className="text-3xl font-bold font-secondary">Sign into your account</div>
       {/* Card Element */}
       <div className="p-8 bg-white rounded-md space-y-6 font-primary sm:w-96">
-        {/* field 1 */}
-        <div>
-          <label className="text-sm font-semibold text-left">Email address</label>
-          <input
-            type="email"
-            id="Email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder=""
-            required
-          ></input>
-        </div>
-        {/* field 2 optional */}
-        <div>
-          <label className="text-sm font-semibold text-left">Password</label>
-          <input
-            type="password"
-            id="Password"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-          ></input>
-        </div>
         {/* sign in button */}
-        <form onSubmit={signInHandler}>
+        <form onSubmit={signInHandler} className="space-y-6">
+          {/* field 1 */}
+          <div>
+            <label className="text-sm font-semibold text-left">Email address</label>
+            <input
+              type="email"
+              id="Email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder=""
+              required
+            ></input>
+          </div>
+          <div>
+            <label className="text-sm font-semibold text-left">Password</label>
+            <input
+              type="password"
+              id="Password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            ></input>
+          </div>
           <button
             type="submit"
             className="h-12 p-2.5 px-4 bg-indigo-800 hover:bg-blue-500 rounded-md w-full"
