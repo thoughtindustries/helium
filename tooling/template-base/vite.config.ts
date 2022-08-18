7469014c127e296868101289af89eb5880a0f2b2
Robert Schillinger <robert.schillinger@thoughtindustries.com>
2022-08-18 18:56:54 +0000

    feat: frontmatter support

--- tooling/template-base/vite.config.ts
+++ tooling/template-base/vite.config.ts
@@ -1,18 +1,6 @@
 import { tiConfig } from '@thoughtindustries/helium-server';
-
 import { defineConfig } from 'vite';
 
 export default defineConfig(async () => {
-  const remarkFrontmatter = await import('remark-frontmatter');
-  const remarkMdxFrontmatter = await import('remark-mdx-frontmatter');
-  const mdx = await import('@mdx-js/rollup');
-
-  const mdxOptions = {
-    remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter.default],
-    rehypePlugins: []
-  };
-
-  tiConfig.plugins.push(mdx.default(mdxOptions));
-
-  return tiConfig;
+  return tiConfig();
 });
