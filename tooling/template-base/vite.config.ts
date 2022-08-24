632a83da91ce4b57ff25358b7ab85719970eae6e
Robert Schillinger <robert.schillinger@thoughtindustries.com>
2022-08-24 11:28:40 -0500

    Clm 5479 frontmatter support (#153)
    * feat: frontmatter support
    
    * fix: revert mdx additions to helium-server

--- tooling/template-base/vite.config.ts
+++ tooling/template-base/vite.config.ts
@@ -1,14 +1,12 @@
 import { tiConfig } from '@thoughtindustries/helium-server';
-
 import { defineConfig } from 'vite';
 
 export default defineConfig(async () => {
   const remarkFrontmatter = await import('remark-frontmatter');
   const remarkMdxFrontmatter = await import('remark-mdx-frontmatter');
   const mdx = await import('@mdx-js/rollup');
-
   const mdxOptions = {
-    remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter.default],
+    remarkPlugins: [remarkFrontmatter.default, remarkMdxFrontmatter.default],
     rehypePlugins: []
   };
 
