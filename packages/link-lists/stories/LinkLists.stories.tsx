c5bd1e415d410f09a99285fc01a27101039fc201
Blake Reimer <sobeksea@gmail.com>
2023-02-21 16:53:39 +0000

    feat: link lists

--- packages/link-lists/stories/LinkLists.stories.tsx
+++ packages/link-lists/stories/LinkLists.stories.tsx
@@ -1,58 +1,63 @@
 import React from 'react';
-import { LinkLists, LinkList } from '../src';
+import { StoryObj } from '@storybook/react';
+import { LinkLists, LinkList, LinkListsProps } from '../src';
 
-export default {
-  title: 'Example/LinkLists'
+export default { component: LinkLists };
+
+type LinkLists = StoryObj<LinkListsProps>;
+
+export const Base: LinkLists = {
+  render: () => (
+    <LinkLists title="Dolor Nullam Mattis Sem">
+      <LinkList label="Category 1">
+        <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
+        <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
+        <LinkList.Link href="/subcategory-link3">List subcategory 3</LinkList.Link>
+      </LinkList>
+
+      <LinkList label="Category 2">
+        <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
+        <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
+      </LinkList>
+
+      <LinkList label="Category 3">
+        <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
+        <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
+        <LinkList.Link href="/subcategory-link3">List subcategory 3</LinkList.Link>
+        <LinkList.Link href="/subcategory-link4">List subcategory 4</LinkList.Link>
+      </LinkList>
+
+      <LinkList label="Category 4">
+        <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
+      </LinkList>
+    </LinkLists>
+  )
 };
 
-export const Base = () => (
-  <LinkLists title="Dolor Nullam Mattis Sem">
-    <LinkList label="Category 1">
-      <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
-      <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
-      <LinkList.Link href="/subcategory-link3">List subcategory 3</LinkList.Link>
-    </LinkList>
+export const WithDisplayCutoff: LinkLists = {
+  render: () => (
+    <LinkLists title="Dolor Nullam Mattis Sem" alternateTitleDisplay>
+      <LinkList label="Category 1" displayCutoff={2}>
+        <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
+        <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
+        <LinkList.Link href="/subcategory-link3">List subcategory 3</LinkList.Link>
+      </LinkList>
 
-    <LinkList label="Category 2">
-      <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
-      <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
-    </LinkList>
+      <LinkList label="Category 2" displayCutoff={2}>
+        <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
+        <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
+      </LinkList>
 
-    <LinkList label="Category 3">
-      <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
-      <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
-      <LinkList.Link href="/subcategory-link3">List subcategory 3</LinkList.Link>
-      <LinkList.Link href="/subcategory-link4">List subcategory 4</LinkList.Link>
-    </LinkList>
+      <LinkList label="Category 3" displayCutoff={2}>
+        <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
+        <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
+        <LinkList.Link href="/subcategory-link3">List subcategory 3</LinkList.Link>
+        <LinkList.Link href="/subcategory-link4">List subcategory 4</LinkList.Link>
+      </LinkList>
 
-    <LinkList label="Category 4">
-      <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
-    </LinkList>
-  </LinkLists>
-);
-
-export const WithDisplayCutoff = () => (
-  <LinkLists title="Dolor Nullam Mattis Sem" alternateTitleDisplay>
-    <LinkList label="Category 1" displayCutoff={2}>
-      <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
-      <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
-      <LinkList.Link href="/subcategory-link3">List subcategory 3</LinkList.Link>
-    </LinkList>
-
-    <LinkList label="Category 2" displayCutoff={2}>
-      <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
-      <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
-    </LinkList>
-
-    <LinkList label="Category 3" displayCutoff={2}>
-      <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
-      <LinkList.Link href="/subcategory-link2">List subcategory 2</LinkList.Link>
-      <LinkList.Link href="/subcategory-link3">List subcategory 3</LinkList.Link>
-      <LinkList.Link href="/subcategory-link4">List subcategory 4</LinkList.Link>
-    </LinkList>
-
-    <LinkList label="Category 4" displayCutoff={2}>
-      <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
-    </LinkList>
-  </LinkLists>
-);
+      <LinkList label="Category 4" displayCutoff={2}>
+        <LinkList.Link href="/subcategory-link1">List subcategory 1</LinkList.Link>
+      </LinkList>
+    </LinkLists>
+  )
+};
