a3fd0cee6f4062dad7f33fdf3a7cc6d0e48887ff
Blake Reimer <sobeksea@gmail.com>
2023-03-10 17:24:39 +0000

    fix: fix for storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,8 @@
-import React, { Suspense, useEffect } from 'react';
-import { I18nextProvider } from 'react-i18next';
 import './style.css';
 import { MockedProvider } from '@apollo/client/testing';
-import { i18n } from '../i18n/i18n';
 import { cookieDecorator } from 'storybook-addon-cookie';
+import withI18next from './i18next';
+import { i18n } from '../i18n/i18n';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -18,21 +17,6 @@ export const parameters = {
   },
   i18n,
   locale: 'en'
-};
-
-const withI18next = (Story, context) => {
-  const { locale } = context.globals;
-
-  useEffect(() => {
-    i18n.changeLanguage(locale);
-  }, [locale]);
-  return (
-    <Suspense fallback={<div>loading translations...</div>}>
-      <I18nextProvider i18n={i18n}>
-        <Story />
-      </I18nextProvider>
-    </Suspense>
-  );
 };
 
 export const globalTypes = {
