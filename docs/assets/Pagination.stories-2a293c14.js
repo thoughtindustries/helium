03ba62ac6fca05d8c3443d454b2492d1124cb331
BReimerTI <98831242+BReimerTI@users.noreply.github.com>
2023-03-14 20:36:53 +0000

    Deploying to storybook-pages from  @ 400f24bc1dc1e99297db90862919cfbdd72f8ea6 🚀

--- docs/assets/Pagination.stories-2a293c14.js
+++
@@ -1,7 +0,0 @@
-import{a as n}from"./jsx-runtime-9707e85d.js";import{P as e}from"./pagination-b905e810.js";import"./index-e0ddb630.js";import"./_commonjsHelpers-87174ba5.js";import"./clsx.m-1229b3e0.js";const d={component:e,title:"Packages/Pagination"},a={render:s=>n(e,{...s}),args:{total:100}};var r,o,t;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: args => <Pagination {...args} />,
-  args: {
-    total: 100
-  }
-}`,...(t=(o=a.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const l=["Base"];export{a as Base,l as __namedExportsOrder,d as default};
-//# sourceMappingURL=Pagination.stories-2a293c14.js.map
---
+++ docs/assets/Pagination.stories-9f21bfe9.js
@@ -0,0 +1,7 @@
+import{a as n}from"./jsx-runtime-9707e85d.js";import{P as e}from"./pagination-b905e810.js";import"./index-e0ddb630.js";import"./_commonjsHelpers-87174ba5.js";import"./clsx.m-1229b3e0.js";const d={component:e,title:"Packages/Pagination"},a={render:s=>n(e,{...s}),args:{total:50}};var r,o,t;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => <Pagination {...args} />,
+  args: {
+    total: 50
+  }
+}`,...(t=(o=a.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const l=["Base"];export{a as Base,l as __namedExportsOrder,d as default};
+//# sourceMappingURL=Pagination.stories-9f21bfe9.js.map
