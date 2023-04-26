2fa166d862b1403e85f88627ace5149c4f7657c1
AlexONeillTI <alex.oneill@thoughtindustries.com>
2023-04-26 17:10:32 +0000

    fix: move RenderNavElements out of scope

--- tooling/template-base-essentials/components/CommunityNav.tsx
+++ tooling/template-base-essentials/components/CommunityNav.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import DiscordLogo from '../renderer/discord.svg';
 import GitHubLogo from '../renderer/github.svg';
 
+const RenderNavElements = (props: {
+  elements: Array<{ text: string; image?: string; link: string }>;
+}) => {
+  const ElementsArray = [];
+  for (let i = 0; i < props.elements.length; i++) {
+    ElementsArray.push(
+      <a href={props.elements[i].link} className="flex">
+        {props.elements[i].image && <img src={props.elements[i].image} />}
+        <div className="text-slate-600 underline px-1 font-semibold">{props.elements[i].text}</div>
+      </a>
+    );
+  }
+  return <>{ElementsArray}</>;
+};
+
 const CommunityNav = () => {
   const NavElementArray = [
     {
@@ -15,23 +30,6 @@ const CommunityNav = () => {
       link: 'https://discord.gg/cTJBX4muVn'
     }
   ];
-
-  const RenderNavElements = (props: {
-    elements: Array<{ text: string; image?: string; link: string }>;
-  }) => {
-    const ElementsArray = [];
-    for (let i = 0; i < props.elements.length; i++) {
-      ElementsArray.push(
-        <a href={props.elements[i].link} className="flex">
-          {props.elements[i].image && <img src={props.elements[i].image} />}
-          <div className="text-slate-600 underline px-1 font-semibold">
-            {props.elements[i].text}
-          </div>
-        </a>
-      );
-    }
-    return <>{ElementsArray}</>;
-  };
 
   return (
     <div className="flex w-full pt-16 pr-10 lg:pr-80 justify-end space-x-3">
