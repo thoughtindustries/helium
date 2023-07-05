44173a85dca7de9a9f5243af544f57496904a611
Lu Jiang <lu.jiang@thoughtindustries.com>
2023-07-05 23:39:07 +0000

    fix: remove atoms from app tailwind config

--- tooling/template-base-essentials/tailwind.config.js
+++ tooling/template-base-essentials/tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: [require('@thoughtindustries/helium-tailwind-preset')],
   content: [
-    './atoms/**/*.{js,jsx,ts,tsx}',
     './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
     './dist/**/*.{js,jsx,ts,tsx}'
