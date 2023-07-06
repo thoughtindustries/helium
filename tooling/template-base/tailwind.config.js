81844730147d35d26c79b1db2c524d0d4bb875de
Lu Jiang <lu.jiang@thoughtindustries.com>
2023-07-06 17:40:57 +0000

    Revert "fix: remove atoms from app tailwind config"
    This reverts commit 44173a85dca7de9a9f5243af544f57496904a611.

--- tooling/template-base/tailwind.config.js
+++ tooling/template-base/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [require('@thoughtindustries/helium-tailwind-preset')],
   content: [
+    './atoms/**/*.{js,jsx,ts,tsx}',
     './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
     './dist/**/*.{js,jsx,ts,tsx}'
