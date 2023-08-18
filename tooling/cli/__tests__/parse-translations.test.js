8c4d509208adef73e179bcae778a8ee7d14dd8fd
AlexONeillTI <alex.oneill@thoughtindustries.com>
2023-08-18 08:10:35 +0000

    chore: update processedTranslations

--- tooling/cli/__tests__/parse-translations.test.js
+++ tooling/cli/__tests__/parse-translations.test.js
@@ -62,66 +62,66 @@ describe('readTranslations', () => {
   });
 });
 
-// describe('processTranslations', () => {
-//   it('should process translations correctly', async () => {
-//     const OP_DIR = path.join(process.cwd(), 'tooling/cli');
+describe('processTranslations', () => {
+  it('should process translations correctly', async () => {
+    const OP_DIR = path.join(process.cwd(), 'tooling/cli');
 
-//     const expectedTranslations = {
-//       en: {
-//         lms: {
-//           'content-access': 'Content Access:',
-//           runs: 'Runs:',
-//           'course-ended': 'Course ended:',
-//           'course.prerequisites': 'You have not met the prerequisites.',
-//           'course-view-details': 'View Details',
-//           'learning-path.view': 'View Learning Path',
-//           'learning-path.continue': 'Continue Learning Path',
-//           'learning-path.start': 'Start Learning Path',
-//           'view-course': 'View %{contentType}',
-//           'continue-course': 'Continue',
-//           'start-course': 'Start %{contentType}',
-//           'not-completed-course': 'Not Completed',
-//           'join-waitlist': 'Join Waitlist',
-//           'dashboard.bookmark-save': 'Save',
-//           'dashboard.unenroll-waitlist': 'Unenroll from Waitlist',
-//           'dashboard.archive-warning':
-//             'Would you like to continue archiving this course? Doing so will remove this from your dashboard as an In Progress item.',
-//           'dashboard.archived': 'Archived',
-//           'bookmark.cancel': 'Cancel',
-//           'bookmark.delete-confirmation': 'Are you sure?',
-//           'bookmark.delete-folder-confirmation':
-//             'This action cannot be undone and all bookmarks in this folder will be deleted.',
-//           'bookmark.view': 'View',
-//           'bookmark.remove': 'Remove Bookmark',
-//           no: 'No',
-//           yes: 'Yes',
-//           'filter-no-courses':
-//             'Sorry, we were unable to find a match for that search term or category, please try again.',
-//           'archive.reinstate': 'Reinstate',
-//           'external-certificate.upload': 'Upload Third-Party Certificate',
-//           'external-certificate.instructions':
-//             'Upload a third-party certificate. Please provide details.',
-//           'external-certificate.cancel': 'Cancel',
-//           'external-certificate.submit': 'Submit',
-//           remove: 'Remove',
-//           'wysiwyg.image': 'Image',
-//           'certificate.issued-date': 'Issue Date',
-//           'certificate.expiration-date': 'Expiration Date'
-//         }
-//       }
-//     };
+    const expectedProcessedTranslations = {
+      en: {
+        lms: {
+          'content-access': 'Content Access:',
+          runs: 'Runs:',
+          'course-ended': 'Course ended:',
+          'course.prerequisites': 'You have not met the prerequisites.',
+          'course-view-details': 'View Details',
+          'learning-path.view': 'View Learning Path',
+          'learning-path.continue': 'Continue Learning Path',
+          'learning-path.start': 'Start Learning Path',
+          'view-course': 'View %{contentType}',
+          'continue-course': 'Continue',
+          'start-course': 'Start %{contentType}',
+          'not-completed-course': 'Not Completed',
+          'join-waitlist': 'Join Waitlist',
+          'dashboard.bookmark-save': 'Save',
+          'dashboard.unenroll-waitlist': 'Unenroll from Waitlist',
+          'dashboard.archive-warning':
+            'Would you like to continue archiving this course? Doing so will remove this from your dashboard as an In Progress item.',
+          'dashboard.archived': 'Archived',
+          'bookmark.cancel': 'Cancel',
+          'bookmark.delete-confirmation': 'Are you sure?',
+          'bookmark.delete-folder-confirmation':
+            'This action cannot be undone and all bookmarks in this folder will be deleted.',
+          'bookmark.view': 'View',
+          'bookmark.remove': 'Remove Bookmark',
+          no: 'No',
+          yes: 'Yes',
+          'filter-no-courses':
+            'Sorry, we were unable to find a match for that search term or category, please try again.',
+          'archive.reinstate': 'Reinstate',
+          'external-certificate.upload': 'Upload Third-Party Certificate',
+          'external-certificate.instructions':
+            'Upload a third-party certificate. Please provide details.',
+          'external-certificate.cancel': 'Cancel',
+          'external-certificate.submit': 'Submit',
+          remove: 'Remove',
+          'wysiwyg.image': 'Image',
+          'certificate.issued-date': 'Issue Date',
+          'certificate.expiration-date': 'Expiration Date'
+        }
+      }
+    };
 
-//     const translations = await readTranslations(OP_DIR);
+    const translations = await readTranslations(OP_DIR);
 
-//     console.log('translations: ', translations);
+    console.log('translations: ', translations);
 
-//     const proccessedResults = await processTranslations(translations, OP_DIR);
+    const proccessedResults = await processTranslations(translations, OP_DIR);
 
-//     console.log('proccessedResults: ', proccessedResults);
+    console.log('proccessedResults: ', proccessedResults);
 
-//     expect(proccessedResults).toEqual(expectedTranslations);
-//   });
-// });
+    expect(proccessedResults).toEqual(expectedProcessedTranslations);
+  });
+});
 
 // describe('writeTranslations', () => {
 //   it('should write translations to a file', async () => {
