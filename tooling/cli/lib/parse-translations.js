8f339dc0370f957158ed27679f6e4b538207c623
AlexONeillTI <alex.oneill@thoughtindustries.com>
2023-08-18 09:34:08 +0000

    chore: check if file exists for path.join(OP_DIR, 'dist');

--- tooling/cli/lib/parse-translations.js
+++ tooling/cli/lib/parse-translations.js
@@ -7,7 +7,7 @@ const fsPromises = require('fs/promises');
 async function processTranslations(TI_TRANSLATIONS, OP_DIR) {
   const FINAL_TRANSLATIONS = {};
   const KEYS_WITH_PLURALS = [];
-  const compiledProjectPath = '/home/runner/work/helium/tooling/cli/dist';
+  const compiledProjectPath = path.join(OP_DIR, 'dist');
 
   console.log('compiledProjectPath: ', compiledProjectPath);
 
