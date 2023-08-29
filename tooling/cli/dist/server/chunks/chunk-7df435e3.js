e3ec81bfc9bf5de96a39223dbabf1b4447937f29
AlexONeillTI <alex.oneill@thoughtindustries.com>
2023-08-29 08:57:32 +0000

    chore: remove dist directory

--- tooling/cli/dist/server/chunks/chunk-7df435e3.js
+++
@@ -1,5 +0,0 @@
-import * as jsxRuntime from 'react/jsx-runtime';
-const Fragment = jsxRuntime.Fragment;
-const jsx = jsxRuntime.jsx;
-const jsxs = jsxRuntime.jsxs;
-export { Fragment as F, jsxs as a, jsx as j };
--- tooling/cli/dist/server/importBuild.mjs
+++
@@ -1,1 +0,0 @@
-console.warn("[vite-plugin-ssr][Warning] importBuild.js is not needed anymore; it has no effect. Remove `import 'dist/server/importBuild.js'` to remove this warning. More infos at https://vite-plugin-ssr.com/importBuild.js");
