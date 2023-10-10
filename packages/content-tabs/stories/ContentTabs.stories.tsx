91f57692eae7890c51dc0d9cbe259a3073ab0d1c
Emiliano Díaz <ediaz@litebox.ai>
2023-10-10 11:18:37 -0300

    feat: added some storybook apollo scaffolding.

--- packages/content-tabs/stories/ContentTabs.stories.tsx
+++ packages/content-tabs/stories/ContentTabs.stories.tsx
@@ -1,6 +1,36 @@
 import { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
-import { ContentTabsProps, ContentTabs } from '../src';
+import { ContentTabs, ContentTabsProps } from '../src';
+import { GetCourseDataDocument, GetLearningPathDataDocument } from '../src/graphql';
+import { ContentKind } from '../src/graphql/global-types';
+
+const mockGetCourseResults = ({ slug }: { slug: string }) => ({
+  request: {
+    query: GetCourseDataDocument,
+    variables: {
+      slug
+    }
+  },
+  result: {
+    data: {
+      CourseGroupBySlug: {}
+    }
+  }
+});
+
+const mockGetLearningPathResults = ({ slug }: { slug: string }) => ({
+  request: {
+    query: GetLearningPathDataDocument,
+    variables: {
+      slug
+    }
+  },
+  result: {
+    data: {
+      LearningPathBySlug: {}
+    }
+  }
+});
 
 const meta: Meta<ContentTabsProps> = {
   component: ContentTabs,
@@ -10,9 +40,20 @@ const meta: Meta<ContentTabsProps> = {
 export default meta;
 type ContentTabs = StoryObj<ContentTabsProps>;
 
+const mockApolloResults = [
+  mockGetCourseResults({ slug: 'course-example' }),
+  mockGetLearningPathResults({ slug: 'course-example' })
+];
 export const Base: ContentTabs = {
   render: args => <ContentTabs {...args} />,
   args: {
-    tabsView: true
+    tabsView: true,
+    slug: 'course-example',
+    contentKind: ContentKind.Course
+  },
+  parameters: {
+    apolloClient: {
+      mocks: mockApolloResults
+    }
   }
 };
