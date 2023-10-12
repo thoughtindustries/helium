91d2f48f9293416ca2ab285a997e9dcf277917e2
Emiliano Díaz <ediaz@litebox.ai>
2023-10-12 00:23:11 -0300

    fix: rever tooling changes for testing

--- tooling/template-base/pages/learn/dashboard.page.tsx
+++ tooling/template-base/pages/learn/dashboard.page.tsx
@@ -5,8 +5,6 @@ import NavBar from '../../components/Navigation/NavBar';
 import LearnerAccess from '../../components/LearnerAccess/LearnerAccess';
 import FeaturedContentComp from '../../components/FeaturedContent/FeaturedContentComp';
 import { HydratedContentItem } from '@thoughtindustries/content';
-import { ContentTabs } from '@thoughtindustries/content-tabs';
-import { ContentKind } from '@thoughtindustries/content/src/graphql/global-types';
 
 export { Page };
 export { documentProps };
@@ -25,7 +23,6 @@ function Page() {
           heading="My Dashboard"
           subtext="Your Dashboard is your game-changing collaborative space where you can view all your learning in one place."
         />
-        <ContentTabs tabsView={true} slug="course-example" contentKind={ContentKind.Course} />
         <LearnerAccess companyHasWaitlistingFeature={true} />
         <FeaturedContentComp
           onAddedToQueue={function (item: HydratedContentItem): Promise<boolean | void> {
