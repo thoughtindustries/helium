f96212d0871a46b18ec844c0ed06ecde1bea8c0b
Robert Schillinger <robert.schillinger@thoughtindustries.com>
2023-11-06 19:36:53 +0000

    feat: allow deployment of development builds

--- tooling/cli/lib/command-modules/deploy.js
+++ tooling/cli/lib/command-modules/deploy.js
@@ -13,6 +13,12 @@ exports.builder = cmd => {
       type: 'string',
       describe: 'Nickname of instance to deploy'
     })
+    .option('d', {
+      alias: 'debug',
+      type: 'boolean',
+      describe: 'Build and deploy Development build of application',
+      default: false
+    })
     .option('k', {
       alias: 'insecure',
       type: 'boolean',
@@ -25,6 +31,7 @@ exports.handler = function (argv) {
   const env = {
     ...process.env,
     INSTANCE_NAME: argv.instance,
+    DEBUG_BUILD: argv.debug,
     NODE_TLS_REJECT_UNAUTHORIZED: argv.insecure ? '0' : '1'
   };
 
